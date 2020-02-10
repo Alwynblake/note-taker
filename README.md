78473ae5ec6f20692e37e75442cf9dc12cd28ae6
Alistair Blake <consultations@me.com>
2020-02-09 21:11:59 -0800

    included middleware

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # note-taker
 backend server using OAuth2
-####OAuth2
+#### OAuth2
 OAuth is an open standard for access delegation. It serves as a way to give users the ability 
 to grant application access to services, without giving the application their password.
 
-####Operating Instructions
+#### Operating Instructions
 If someone were to download your repo (above), what steps do they need to take to run the application
 * `npm start`
 *  Endpoint: /signup
---
+++ src/auth/router.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./users-model.js');
+const auth = require('./middleware.js');
+const oauth = require('./oauth/github.js');
+
+authRouter.post('/signup', (req, res, next) => {
+  let user = new User(req.body);
+  user.save()
+      .then( (user) => {
+        req.token = user.generateToken();
+        req.user = user;
+        res.set('token', req.token);
+        res.cookie('auth', req.token);
+        res.send(req.token);
+      }).catch(next);
+});
+// eslint-disable-next-line no-unused-vars
+authRouter.post('/signin', auth, (req, res, next) => {
+  res.cookie('auth', req.token);
+  res.send(req.token);
+});
+
+authRouter.get('/oauth', oauth);
+
+module.exports = authRouter;
